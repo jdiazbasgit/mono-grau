68edda78d72312fe6879f72aaa4c72c592cee7b2
Your Name <you@example.com>
2025-04-23 19:57:43 +0200

    web

--- src/main/java/com/amipem/mono/MonoApplication.java
+++ src/main/java/com/amipem/mono/MonoApplication.java
@@ -2,12 +2,21 @@ package com.amipem.mono;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import acceos.recrale.nuevo.Accesos2025Application;
 
 @SpringBootApplication
-public class MonoApplication {
+public class MonoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonoApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+    {
+        return application.sources(MonoApplication.class);
+    }
 }
